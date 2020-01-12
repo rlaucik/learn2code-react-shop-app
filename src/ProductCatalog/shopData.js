33cf12add1707c98ab6f318a64f9a39989399014
Radomir Laucik <laucik@gmail.com>
2020-01-12 21:01:17 +0100

    Add data to products

--- src/ProductCatalog/shopData.js
+++ src/ProductCatalog/shopData.js
@@ -22,47 +22,55 @@ export const products = [
         "name": "iPhone 11 64GB Black",
         "price": 819.00,
         "thumbnail": "https://www.istores.sk/iphone-11-64gb-black_ig97066.jpg?attname=thumbnail_490&attpedid=52",
-        "categoryId": "1"
+        "categoryId": "1",
+        "id": "1",
     },
     {
         "name": "iPhone XS 64GB Silver",
         "price": 888.00,
         "thumbnail": "https://www.istores.sk/iphone-xs-64gb-silver_ig81366.jpg?attname=thumbnail_490&attpedid=52",
-        "categoryId": "1"
+        "categoryId": "1",
+        "id": "2",
     },
     {
         "name": "iPhone 8 Plus 128GB Space Gray",
         "price": 819.00,
         "thumbnail": "https://www.istores.sk/iphone-8-plus-128gb-space-grey_ig97111.jpg?attname=thumbnail_490&attpedid=52",
-        "categoryId": "1"
+        "categoryId": "1",
+        "id": "3",
     },
     {
         "name": "MacBook Pro 13\" i5 2.3GHz 8GB 128GB Space Gray SK",
         "price": 1225.00,
-        "categoryId": "2"
+        "categoryId": "2",
+        "id": "4",
     },
     {
         "name": "iMac 21.5\" FHD i5 2.3GHz 8GB 1TB Iris Plus Graphics 640 SK",
         "price": 1179.00,
         "thumbnail": "https://www.istores.sk/imac-21-5-fhd-i5-2-3ghz-8gb-1tb-iris-plus-graphics-640-sk_ig68530.jpg?attname=thumbnail_490&attpedid=52",
-        "categoryId": "2"
+        "categoryId": "2",
+        "id": "5",
     },
     {
         "name": "MacBook Air 13\" i5 1.8GHz 8GB 128GB flash SK",
         "price": 819.00,
         "thumbnail": "https://www.istores.sk/macbook-air-13-i5-1-8ghz-8gb-128gb-flash-sk-galeria-1-small_ies999714.jpg",
-        "categoryId": "2"
+        "categoryId": "2",
+        "id": "6",
     },
     {
         "name": "Apple AirPods Pro",
         "price": 279.00,
         "thumbnail": "https://www.istores.sk/apple-airpods-pro_ig98781.jpg?attname=thumbnail_490&attpedid=52",
-        "categoryId": "3"
+        "categoryId": "3",
+        "id": "7",
     },
     {
         "name": "Apple TV 4K 64GB",
         "price": 219.00,
         "thumbnail": "https://www.istores.sk/apple-tv-4k-64gb-galeria-5-small_ies1031074.jpg",
-        "categoryId": "3"
+        "categoryId": "3",
+        "id": "8",
     }
 ]
